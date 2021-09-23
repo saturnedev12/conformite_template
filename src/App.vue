4ab5993f8901233ab47a1e6fa0eaf21bc9ed2d25
saturne <saturne.dev12@gmail.com>
2021-09-23 16:29:23 +0000

    app bar

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,32 @@
   <div>
     {{ info }}
     <v-app id="inspire" v-show="isLogin">
-      
-      <v-navigation-drawer v-model="drawer" app color="orange">
+
+      <v-navigation-drawer v-model="drawer" app>
         <!-- la bar -->
        <NavigationBar/>
       </v-navigation-drawer>
 
       <v-app-bar app>
         <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+           
+        <!--<v-toolbar-title>Application</v-toolbar-title>-->
 
-        <v-toolbar-title>Application</v-toolbar-title>
+        <v-row justify="start">
+            <v-btn class="ma-2" color="secondary" >NOUVEAU CLIENT PENDING</v-btn>
+            <v-btn class="ma-2" color="secondary" >CLIENT EN ALERTE</v-btn>
+            <v-btn class="ma-2" color="secondary" >CLIENT EN ATTENTE D'UPGRATE</v-btn>
+            <v-btn class="ma-2" color="secondary" >CONTROLE ALEATOIRE</v-btn>
+        </v-row>
+
+          <v-img
+                max-height="50"
+                max-width="50"
+                src="./assets/assets_images_uniskip_logo.svg"
+          ></v-img>
       </v-app-bar>
+
+
       <v-main>
         <!-- Core Page -->
         <router-view/>
@@ -45,12 +60,17 @@
           this.isLogin = await network.login(payload.email,payload.password);
           console.log("yes");
           console.log(this.isLogin);
-      },
+      }, 
+      showBarConf(val){
+        this.showBarConf = val;
+      }
     },
     data: () => ({
-      drawer: null, 
+      drawer: null,
       isLogin:true,
       info: null,
+      showBarConf : false
       }),
+   
   }
 </script>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,6 +1,108 @@
 <template>
-    <v-list nav dense >
 
+   <v-list>
+        <v-list-item-group v-model="model" color="orange">
+          <router-link to="/" style="text-decoration: none; color: inherit;">
+            <v-list-item style="background-color: ">
+                <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                <v-list-item-title>Accueil</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+           </router-link>
+          <v-divider></v-divider>
+
+            <router-link to="/conformite" style="text-decoration: none; color: inherit;">
+            <v-list-item style="background-color: ">
+                <v-list-item-icon>
+                <v-icon>mdi-star-check</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                <v-list-item-title>Conformité</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            </router-link>
+
+          <v-divider></v-divider>
+          <v-list-item style="background-color: ">
+            <v-list-item-icon>
+              <v-icon>mdi-alert</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Alertes</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item style="background-color: ">
+            <v-list-item-icon>
+              <v-icon>mdi-account-group</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Gestion client</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item style="background-color: ">
+            <v-list-item-icon>
+              <v-icon>mdi-database</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Base et profiles</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item style="background-color: ">
+            <v-list-item-icon>
+              <v-icon>mdi-cash-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Cash</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item style="background-color: ">
+            <v-list-item-icon>
+              <v-icon>mdi-chart-bell-curve-cumulative</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Indicateurs</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item style="background-color: ">
+            <v-list-item-icon>
+              <v-icon>mdi-face-agent</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>CRM</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item style="background-color: ">
+            <v-list-item-icon>
+              <v-icon>mdi-piggy-bank-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Comptabilité</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item style="background-color: ">
+            <v-list-item-icon>
+              <v-icon>mdi-account-supervisor-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Superviseur</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+
+     <!--<v-list nav dense >
+            
             <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
                 <router-link to="/">
                 <v-list-item>
@@ -21,10 +123,15 @@
                 </router-link>
             </v-list-item-group>
 
-    </v-list>
+    </v-list>-->
 </template>
 <script>
 export default {
-    name:"NavigationBar"
+    name:"NavigationBar",
+    methods:{
+        showBar(){
+            this.$emit;
+        }
+    }
 }
 </script>
