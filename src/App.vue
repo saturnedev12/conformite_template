77ffa7a0c24abbb360c7d2e32817920cb105f07c
saturne <saturne.dev12@gmail.com>
2021-09-23 14:08:01 +0000

    separation du drawer

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,10 @@
   <div>
     {{ info }}
     <v-app id="inspire" v-show="isLogin">
-      <v-navigation-drawer
-        v-model="drawer"
-        app color="orange">
+      
+      <v-navigation-drawer v-model="drawer" app color="orange">
         <!-- la bar -->
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item-group
-            v-model="group"
-            active-class="deep-purple--text text--accent-4"
-          >
-            
-            <router-link to="/">
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-home</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Home</v-list-item-title>
-              </v-list-item>
-            </router-link>
-            
-            <router-link to="/conformite">
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon>mdi-account</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-title>Conformite</v-list-item-title>
-                </v-list-item>
-            </router-link>
-
-            
-
-          
-
-          </v-list-item-group>
-        </v-list>
+       <NavigationBar/>
       </v-navigation-drawer>
 
       <v-app-bar app>
@@ -64,10 +31,12 @@
 <script>
   import Login from './views/Login.vue'
   import Network from './class/Network'
+  import NavigationBar from './components/NavigationBar.vue'
   const network = new Network();
   export default {
     components:{
-      Login
+      Login,
+      NavigationBar,
     },
     methods: {
       async checkUser(payload){
@@ -82,22 +51,6 @@
       drawer: null, 
       isLogin:true,
       info: null,
-      token : localStorage.token
       }),
-    async mounted(){
-       if (localStorage.token != "" && localStorage.token != null && (typeof localStorage.token === 'string' || localStorage.token instanceof String)) {
-         this.isLogin = !this.isLogin;
-       }
-       //this.isLogin = network.getFonctionnalite();
-      //console.log(network.hello());
-      console.log(`test api ${ await network.getFonctionnalite()}`);
-
-    },
-    watch:{
-      name(){
-        this.allow();
-      }
-    }
-    
   }
 </script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-list nav dense >
+
+            <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+                <router-link to="/">
+                <v-list-item>
+                    <v-list-item-icon>
+                    <v-icon>mdi-home</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Home</v-list-item-title>
+                </v-list-item>
+                </router-link>
+                
+                <router-link to="/conformite">
+                    <v-list-item>
+                    <v-list-item-icon>
+                        <v-icon>mdi-account</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Conformite</v-list-item-title>
+                    </v-list-item>
+                </router-link>
+            </v-list-item-group>
+
+    </v-list>
+</template>
+<script>
+export default {
+    name:"NavigationBar"
+}
+</script>
