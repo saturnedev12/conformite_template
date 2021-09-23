e9ef3b87ed4db8fe5bd67a9e6f9859c538fcf269
BOKA KOFFI AQUEGNAN JOCELYN <jocelynboka@gmail.com>
2021-09-23 13:53:33 +0200

    teste

--- src/views/conformite/banque/Banque.vue
+++ src/views/conformite/banque/Banque.vue
@@ -1,6 +1,6 @@
 <template>
    <v-card height="230" elevation="2" outlined class="ma-2">
-              <h4>BANQUE</h4>
+              <h4>BANQUES</h4>
               <v-divider></v-divider>
     </v-card>
 </template>
