27d967c81cfa75a063be949733357ef490a90c7c
Axelle Cheryl Kongoue <axelle.kongoue@uniskip.com>
2021-09-24 11:27:54 +0000

    hemdal

--- src/views/conformite/hemdal/Hemdal.vue
+++ src/views/conformite/hemdal/Hemdal.vue
@@ -1,12 +1,61 @@
 <template>
-     <v-card height="400" elevation="2" outlined class="ma-2">
-              <h4>HEMDAL</h4>
-              <v-divider></v-divider>
-    </v-card>
+  <v-card height="400" elevation="2" outlined class="ma-2">
+    <h4>HEMDAL</h4>
+    <v-divider></v-divider>
+    <v-flex>
+      <v-btn class="ma-2" color="red" outlined>NEGATIF</v-btn>
+    </v-flex>
+    <v-flex>
+      <v-btn class="ma-2" color="green" outlined>POSITIF</v-btn>
+    </v-flex>
+    <v-btn block outlined disabled>CONFORMITE HEMDAL</v-btn>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="hemdal_item">BASE 1
+            <v-space>KO</v-space>
+        </v-list-item-title>
+        <v-divider></v-divider>
+        <v-list-item-title class="hemdal_item">BASE 2
+            <v-space>KO</v-space>
+        </v-list-item-title>
+        <v-divider></v-divider>
+        <v-list-item-title class="hemdal_item">
+            BASE 3
+            <v-space>KO</v-space>
+        </v-list-item-title>
+        <v-divider></v-divider>
+        <v-list-item-title class="hemdal_item">BASE 4
+            <v-space>KO</v-space>
+        </v-list-item-title>
+        <v-divider></v-divider>
+        <v-list-item-title class="hemdal_item">BASE 5
+            <v-space>KO</v-space>
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item>
+      <v-list-item-content>
+        <v-divider></v-divider>
+        <v-list-item-title class="hemdal_item"
+          >CONTRAT
+          <v-btn outlined x-small color="orange">OK</v-btn>
+        </v-list-item-title>
+        <v-divider></v-divider>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        name:"Hemdal"
-    }
+export default {
+  name: "Hemdal",
+};
 </script>
+
+<style>
+.hemdal_item{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+</style>
