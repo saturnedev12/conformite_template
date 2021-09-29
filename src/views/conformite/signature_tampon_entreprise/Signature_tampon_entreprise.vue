59ea14b5781e26d159aedfa255927cd190d14faf
Axelle Cheryl Kongoue <axelle.kongoue@uniskip.com>
2021-09-29 18:48:10 +0000

    signature

--- src/views/conformite/signature_tampon_entreprise/Signature_tampon_entreprise.vue
+++ src/views/conformite/signature_tampon_entreprise/Signature_tampon_entreprise.vue
@@ -1,12 +1,63 @@
 <template>
-    <v-card height="200" elevation="2" outlined class="ma-2"
+  <v-card height="200" elevation="2" outlined class="ma-2">
+    <h4>SIGNATURE ET TAMPON ENTREPRISE</h4>
+    <v-divider></v-divider>
+    <v-row :align="align" no-gutters>
+          <v-col cols="4">
+            <v-col cols="12">
+            </v-col>
+          </v-col>
+
+          <v-col cols="6">
+            <v-col cols="12">
+              <label
+                class="form-check-label"
+                for="defaultCheck1"
+                style="font-size: 13px"
+                ><b>Signature (CGU)</b></label
               >
-              <h4>SIGNATURE ET TAMPON ENTREPRISE</h4>
-              <v-divider></v-divider>
-            </v-card>
+            </v-col>
+          </v-col>
+
+          <v-col cols="2">
+            <v-switch
+              v-model="ex11"
+              label=""
+              color="success"
+              value="success"
+              hide-details
+            ></v-switch>
+          </v-col>
+          <v-col cols="4">
+            <v-col cols="12">
+            </v-col>
+          </v-col>
+
+          <v-col cols="6">
+            <v-col cols="12">
+              <label
+                class="form-check-label"
+                for="defaultCheck1"
+                style="font-size: 13px"
+                ><b>Tampon attestaion d'activités</b></label
+              >
+            </v-col>
+          </v-col>
+
+          <v-col cols="2">
+            <v-switch
+              v-model="ex11"
+              label=""
+              color="success"
+              value="success"
+              hide-details
+            ></v-switch>
+          </v-col>
+        </v-row>
+  </v-card>
 </template>
 <script>
 export default {
-    name:"Signature_tampon_entreprise"
-}
+  name: "Signature_tampon_entreprise",
+};
 </script>
